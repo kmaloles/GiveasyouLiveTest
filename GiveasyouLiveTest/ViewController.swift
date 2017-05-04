760926183cecd9ae2dffdecd5f1befb50a27e3e2
kmaloles <kmaloles@ticketbiscuit.com>
2017-05-04 03:27:25 -0500

    Initial Commit

---
+++ GiveasyouLiveTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GiveasyouLiveTest
+//
+//  Created by Kevin Maloles on 5/4/17.
+//  Copyright © 2017 Kevin Maloles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
